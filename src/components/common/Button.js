8747e5ea0b1978d5d8e391c48fc70a449b80f730
AkkaFranKebne <ewawrobel@gmail.com>
2018-11-30 11:02:52 +0100

    a basic styled component

---
+++ src/components/common/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  background: white;
+  border: 5px solid black;
+  font-size: 20px;
+  color: blue;
+`;
+
+export default Button;
